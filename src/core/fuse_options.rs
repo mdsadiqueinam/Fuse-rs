ae4654128e71158116b1ccc5800529448deb5272
Md Sadique Inam <md.sadique32@gmail.com>
2025-05-14 15:58:19 +0530

    Refactor FuseOptions structure and improve type handling for key names

--- src/core/fuse_options.rs
+++ src/core/fuse_options.rs
@@ -1,10 +1,11 @@
 use crate::helpers::get;
-use serde_json::Value;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::borrow::Cow;
 
 /// Default sort function for search results
 fn default_sort_fn(a: &SearchResult, b: &SearchResult) -> i8 {
-    if a.score == b.score {
+    if (a.score - b.score).abs() < f64::EPSILON {
         if a.idx < b.idx { -1 } else { 1 }
     } else {
         if a.score < b.score { -1 } else { 1 }
@@ -22,130 +23,92 @@ fn default_get_fn_wrapper() -> fn(&Value, &Vec<String>) -> Option<get::GetValue>
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum FuseOptionKeyObjectName {
-    String(String),
-    StringArray(Vec<String>),
+pub enum FuseOptionKeyName<'a> {
+    String(Cow<'a, str>),
+    StringArray(Vec<Cow<'a, str>>),
 }
 
-/// Represents a key object with optional weight and a custom getter function
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct FuseOptionKeyObject {
-    pub name: FuseOptionKeyObjectName, // Represents `string | string[]` as a vector of strings
-    pub weight: Option<f64>, // Optional weight
+pub type FuseKeyValueGetter = Option<fn(&Value) -> &str>;
 
-    #[serde(skip_serializing, skip_deserializing)]
-    pub get_fn: Option<fn(&Value) -> Vec<String>>, // Optional getter function
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FuseOptionKeyObject<'a> {
+    pub name: FuseOptionKeyName<'a>,
+    pub weight: Option<f64>,
+
+    #[serde(skip)]
+    pub get_fn: FuseKeyValueGetter,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
-pub enum FuseOptionKey {
-    KeyObject(FuseOptionKeyObject),
-    String(String),
-    StringArray(Vec<String>),
+pub enum FuseOptionKey<'a> {
+    KeyObject(FuseOptionKeyObject<'a>),
+    String(Cow<'a, str>),
+    StringArray(Vec<Cow<'a, str>>),
 }
 
-/// Configuration options for Fuse.js
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct FuseOptions {
-    // BasicOptions
-    /// When `true`, the algorithm continues searching to the end of the input even if a perfect
-    /// match is found before the end of the same input.
-    pub is_case_sensitive: bool,
-    
-    /// When `true`, the algorithm will ignore diacritics (accents) in comparisons
-    pub ignore_diacritics: bool,
-    
-    /// When true, the search result will include the score
-    pub include_score: bool,
-   
-    /// List of properties that will be searched. This also supports nested properties.
-    pub keys: FuseOptionKey,
-    
-    /// Whether to sort the result list, by score
-    pub should_sort: bool,
-
-    /// Sort function for search results
-    #[serde(skip_serializing, skip_deserializing, default = "default_sort_fn_wrapper")]
-    pub sort_fn: fn(&SearchResult, &SearchResult) -> i8,
-
-    // MatchOptions
-    /// Whether the matches should be included in the result set. When `true`, each record in the result
-    /// set will include the indices of the matched characters.
-    /// These can consequently be used for highlighting purposes.
-    pub include_matches: bool,
-    
-    /// When `true`, the matching function will continue to the end of a search pattern even if
-    /// a perfect match has already been located in the string.
-    pub find_all_matches: bool,
-    
-    /// Minimum number of characters that must be matched before a result is considered a match
-    pub min_match_char_length: usize,
-
-    // FuzzyOptions
-    /// Approximately where in the text is the pattern expected to be found?
-    pub location: usize,
-    
-    /// At what point does the match algorithm give up. A threshold of '0.0' requires a perfect match
-    /// (of both letters and location), a threshold of '1.0' would match anything.
-    pub threshold: f64,
-
-    /// Determines how close the match must be to the fuzzy location (specified above).
-    /// An exact letter match which is 'distance' characters away from the fuzzy location
-    /// would score as a complete mismatch. A distance of '0' requires the match be at
-    /// the exact location specified; a threshold of '1000' would require a perfect match
-    /// to be within 800 characters of the fuzzy location to be found using a 0.8 threshold.
-    pub distance: usize,
-
-    // AdvancedOptions
-    /// When `true`, it enables the use of unix-like search commands
-    pub use_extended_search: bool,
-
-    /// The get function to use when fetching an object's properties.
-    /// The default will search nested paths *ie foo.bar.baz*
-    #[serde(skip_serializing, skip_deserializing, default = "default_get_fn_wrapper")]
-    pub get_fn: fn(&Value, &Vec<String>) -> Option<get::GetValue>,
-    
-    /// When `true`, search will ignore `location` and `distance`, so it won't matter
-    /// where in the string the pattern appears.
-    pub ignore_location: bool,
-    
-    /// When `true`, the calculation for the relevance score (used for sorting) will
-    /// ignore the field-length norm.
-    pub ignore_field_norm: bool,
-    
-    /// The weight to determine how much field length norm effects scoring.
-    pub field_norm_weight: usize,
+impl<'a> Default for FuseOptionKey<'a> {
+    fn default() -> Self {
+        FuseOptionKey::StringArray(Vec::new())
+    }
 }
 
 /// Represents a search result item
+#[derive(Debug)]
 pub struct SearchResult {
     pub score: f64,
     pub idx: usize,
 }
 
-impl Default for FuseOptions {
+/// Configuration options for Fuse.js
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FuseOptions<'a> {
+    // BasicOptions
+    pub is_case_sensitive: bool,
+    pub ignore_diacritics: bool,
+    pub include_score: bool,
+    pub keys: FuseOptionKey<'a>,
+    pub should_sort: bool,
+
+    #[serde(skip, default = "default_sort_fn_wrapper")]
+    pub sort_fn: fn(&SearchResult, &SearchResult) -> i8,
+
+    // MatchOptions
+    pub include_matches: bool,
+    pub find_all_matches: bool,
+    pub min_match_char_length: usize,
+
+    // FuzzyOptions
+    pub location: usize,
+    pub threshold: f64,
+    pub distance: usize,
+
+    // AdvancedOptions
+    pub use_extended_search: bool,
+
+    #[serde(skip, default = "default_get_fn_wrapper")]
+    pub get_fn: fn(&Value, &Vec<String>) -> Option<get::GetValue>,
+
+    pub ignore_location: bool,
+    pub ignore_field_norm: bool,
+    pub field_norm_weight: usize,
+}
+
+impl<'a> Default for FuseOptions<'a> {
     fn default() -> Self {
         FuseOptions {
-            // BasicOptions defaults
             is_case_sensitive: false,
             ignore_diacritics: false,
             include_score: false,
-            keys: FuseOptionKey::StringArray(Vec::new()),
+            keys: FuseOptionKey::default(),
             should_sort: true,
             sort_fn: default_sort_fn,
-
-            // MatchOptions defaults
             include_matches: false,
             find_all_matches: false,
             min_match_char_length: 1,
-
-            // FuzzyOptions defaults
             location: 0,
             threshold: 0.6,
             distance: 100,
-
-            // AdvancedOptions defaults
             use_extended_search: false,
             get_fn: get::get,
             ignore_location: false,
